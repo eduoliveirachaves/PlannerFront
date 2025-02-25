c356488f1cea3c804cf837734202ec550bdf9692
Eduardo de Oliveira Chaves <eduardodeoliveirachaves7@gmail.com>
2025-02-25 04:40:06 +0000

    proxy, cookies configs

--- src/api/Main.ts
+++ src/api/Main.ts
@@ -1,37 +1,71 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000/api",
+  withCredentials: true,
 });
 
 export async function login(email: string, password: string) {
   try {
-    const response = await api
-    .post("/auth/login", {
-      email,
-      password,
+    const response = await api.post(
+      "/auth/login",
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true, // Ensure credentials are included
+      },
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.error("Login failed:", error);
+    throw error;
+  }
+}
+
+export async function registerUser(
+  name: string,
+  email: string,
+  password: string,
+) {
+  try {
+    const response = await api.post(
+      "/user/register",
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      },
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function test() {
+  try {
+    const response = await api.get("/user/me", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
     });
 
     console.log(response.data);
   } catch (error) {
     console.error(error);
     throw error;
-    }
-}
-
-export function registerUser(name: string, email: string, password: string) {
-  api
-    .post("/user/register", {
-      name,
-      email,
-      password,
-    }, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then((response) => {
-      console.log(response.data);
-    });
+  }
 }
 export default api;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,19 @@
+import { test } from '../api/Main';
+
+
 export default function Dashboard() {
+
+  const handleTest = () => {
+    console.log('test');
+    test();
+  }
+
+
   return (
     <div className="dashboard">
       <h1>Welcome to the Dashboard</h1>
       <p>Protected Route</p>
+      <button onClick={handleTest}>TEST</button>
     </div>
   );
 }
